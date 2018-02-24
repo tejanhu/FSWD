6407ddc1bdac6be82a7053eb04b528b9408c87b2
Hussein <tejanahmedhu@gmail.com>
2018-02-24 22:54:08 +0000

    completed JavaScript Stalker Exercise

---
+++ Full-Stack/Front-End/JavaScript/exScript.js
@@ -0,0 +1,9 @@
+var fName = prompt("What is your first name?");
+
+var lName = prompt("What is your last name?");
+
+console.log("Your full name is " + fName + " " + lName);
+
+var age = prompt("How old are you?");
+
+console.log("You are " + age + " " + "years old");
