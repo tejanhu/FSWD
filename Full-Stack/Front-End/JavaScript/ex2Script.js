f0509081b2518291bc723c83aa9f2b2d592ccf68
Hussein <tejanahmedhu@gmail.com>
2018-02-25 00:02:29 +0000

    added JavaScript Age Calculator Exercise 2

---
+++ Full-Stack/Front-End/JavaScript/ex2Script.js
@@ -0,0 +1,19 @@
+var age = prompt("What is your age?");
+
+//NEG NUMBER CONDITIONAL TEST
+if(age < 0){
+	console.log("Error: Negative!");
+}
+
+else if(age == 21){
+	console.log("happy 21st birthday!!");
+}
+// ODD NUMBER CONDITIONAL TEST 
+else if((age % 2) === 1){
+	console.log("your age is odd!");
+}
+
+// PERFECT SQUARE NUMBER CONDITIONAL TEST 
+if(age % Math.sqrt(age) == 0){
+	console.log("perfect square!");
+}
