c65862f37cf2527cce8a6ac1b51d801e8edc1613
Hussein <tejanahmedhu@gmail.com>
2018-02-25 11:56:08 +0000

    edited JavaScript Guessing game

--- Full-Stack/Front-End/JavaScript/game.js
+++ Full-Stack/Front-End/JavaScript/game.js
@@ -2,8 +2,19 @@
 var secretNumber = 4; 
 
 //ask user for guess
-var guess = prompt("Guess a number");
-alert(guess);
+var stringGuess = prompt("GUESS A NUMBER");
+var guess = Number(stringGuess);
 
 //check guess
+if(guess === secretNumber){
+	alert("YOU GOT IT RIGHT!");
+}
+//otherwise, check if higher
+else if(guess > secretNumber){
+	alert("TOO HIGH. GUESS AGAIN!")
+}
 
+//otherwise, check if lower
+else{
+	alert("TOO LOW. GUESS AGAIN!")
+}
