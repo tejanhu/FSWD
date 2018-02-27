fd8b267bb71e07174e3d285eca6969a853bf8c1e
Hussein <tejanahmedhu@gmail.com>
2018-02-27 01:09:22 +0000

    added Code-Along while loop 'Are we there yet?'

---
+++ Full-Stack/Front-End/JavaScript/annoy.js
@@ -0,0 +1,22 @@
+// var answer = prompt("Are we there yet?");
+// while(answer !== "yes" && answer!== "yeah"){
+// 	var answer = prompt("Are we there yet?")
+
+// }
+
+// alert("YAY, WE MADE IT!!!");
+
+// if(answer === "yes"){
+// 	alert("YAY, We made it!");
+// }else{
+//}
+
+// VERSION II
+
+var answer = prompt("Are we there yet?");
+while(answer.indexOf("yes") ===-1){
+	var answer = prompt("Are we there yet?")
+
+}
+
+alert("YAY, WE MADE IT!!!");
