85b3c0a77afc9da0bf78e86ab943d1a19652ac0a
Hussein <tejanahmedhu@gmail.com>
2018-04-08 22:24:09 +0100

    refactored ScoreKeeper exercise

--- Full-Stack/Front-End/JavaScript/ScoreKeeper.js
+++ Full-Stack/Front-End/JavaScript/ScoreKeeper.js
@@ -3,7 +3,8 @@ var p2Button = document.getElementById("p2");
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
 var resetButton = document.querySelector("#reset");
-var limitSet = document.querySelector("#limitSet");
+var numInput = document.querySelector("input");
+var winningScoreDisplay = document.querySelector("p span");
 var gameOver = false;
 
 var p1Score = 0;
@@ -14,28 +15,42 @@ var winningScore = 5;
 p1Button.addEventListener("click", function(){
 	if(!gameOver){
 		p1Score++;	
+		if(p1Score === winningScore){
+			p1Display.classList.add("winner");
+			gameOver = true;
+		}
+		p1Display.textContent = p1Score;
 	}
-	if(p1Score === winningScore){
-		gameOver = true;
-	}
-	p1Display.textContent = p1Score;
 });
 
 p2Button.addEventListener("click", function(){
 	if(!gameOver){
 		p2Score++;	
+		if(p2Score === winningScore){
+			p2Display.classList.add("winner");
+			gameOver = true;
+		}
+		p2Display.textContent = p2Score;
 	}
-	if(p2Score === winningScore){
-		gameOver = true;
-	}
-	p2Display.textContent = p2Score;
 });
 
 resetButton.addEventListener("click", function(){
+	reset();
+});
+
+function reset(){
 	p1Score = 0;
 	p2Score = 0;
-	p1Display.textContent = p1Score;
-	p2Display.textContent = p2Score;
+	p1Display.textContent = 0;
+	p2Display.textContent = 0;
+	p1Display.classList.remove("winner");
+	p2Display.classList.remove("winner");
+	gameOver = false;
+}
 
-})
+numInput.addEventListener("change", function(){
+	winningScoreDisplay.textContent = this.value;
+	winningScore = Number(this.value);
+	reset();
+});
 
