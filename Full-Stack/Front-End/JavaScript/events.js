e047965f9bf4a3bdb4d5f8ef12fdc3f10dfa0790
Hussein <tejanahmedhu@gmail.com>
2018-04-08 16:21:32 +0100

    REFACTORED events exercise with toggle class shorter solution

--- Full-Stack/Front-End/JavaScript/events.js
+++ Full-Stack/Front-End/JavaScript/events.js
@@ -1,14 +1,7 @@
 var button = document.querySelector('button');
-var isPurple = false;
-button.addEventListener("click", function(){
-	
-	if(isPurple){
-		document.querySelector('body').style.background = "white";
-	}
-	else{
-		document.querySelector('body').style.background = "purple";
-	}
-	isPurple = !isPurple;
+
+button.addEventListener("click", function(){	
+	document.body.classList.toggle('purple');	
 });
 
 
