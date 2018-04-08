ed0934c11ac46382f403709d54d5bcc2bd90c3f6
Hussein <tejanahmedhu@gmail.com>
2018-04-08 16:16:35 +0100

    REFACTORED events exercise with isPurple shorter solution

--- Full-Stack/Front-End/JavaScript/events.js
+++ Full-Stack/Front-End/JavaScript/events.js
@@ -4,12 +4,11 @@ button.addEventListener("click", function(){
 	
 	if(isPurple){
 		document.querySelector('body').style.background = "white";
-		isPurple = false;
 	}
 	else{
 		document.querySelector('body').style.background = "purple";
-		isPurple = true;
 	}
+	isPurple = !isPurple;
 });
 
 
