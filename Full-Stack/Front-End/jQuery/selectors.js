a8f19aca2a8f1e9609f1dd09fad18413717bb4ce
Hussein <tejanahmedhu@gmail.com>
2018-04-09 07:15:14 +0100

    added selectors jQuery exercise

---
+++ Full-Stack/Front-End/jQuery/selectors.js
@@ -0,0 +1,10 @@
+$("div").css({
+	background: "purple"
+});
+$("div.highlight").css("width", "200px");
+$("#third").css({
+	border: "4px solid orange"
+});
+$("div:first-of-type").css({
+	color: "pink"
+});
