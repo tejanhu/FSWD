ccac1ba29e387aaf427e753d7560fab37c9bf7b2
Hussein <tejanahmedhu@gmail.com>
2020-02-04 03:26:37 +0000

    another dope comment express.js

--- Full-Stack/Back-End/ExpressJS/FirstExpressApp/app.js
+++ Full-Stack/Back-End/ExpressJS/FirstExpressApp/app.js
@@ -9,14 +9,34 @@ app.get("/", function(request, response){
 // "/bye" => "Goodbye!"
 app.get("/bye", function(request, response){
     response.send("Goodbye!");
-})
+});
 
 // "/dog" => "MEOW!"
 app.get("/dog", function(request, response){
     response.send("MEOW!");
-})
+    console.log("Someone made a request to /dog");
+});
+
+app.get("/r/:subredditName", function(request, response){
+    var subreddit = request.params.subredditName;
+    response.send("WELCOME TO THE " + subreddit.toUpperCase() + " SUBREDDIT!");
+});
+
+app.get("/r/:subredditName/comments/:id/:title/", function(request, response){
+    console.log(request.params);
+    response.send("welcome to the comments page!");
+});
+
+
+app.get("*", function(request, response){
+    response.send("You are a star!");
+});
+
+
+
+
 // Tell express to listen for requests (start server)
 
 app.listen(3000, function(){
     console.log("Listening on port 3000");
-})
+});
