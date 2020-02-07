f5d706fef342710d16ea64ec3b471fd394568ab6
Hussein <tejanahmedhu@gmail.com>
2020-02-07 23:54:08 +0000

    finally got EJSDemo css partials to work

--- Full-Stack/Back-End/ExpressJS/MoreExpress/EJSDemo/app.js
+++ Full-Stack/Back-End/ExpressJS/MoreExpress/EJSDemo/app.js
@@ -1,15 +1,26 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
-    res.render("home.ejs");
+    res.render("home");
 }); 
 
 app.get("/fallinlovewith/:thing", function(req, res){
     var thing = req.params.thing;
-    res.render("love.ejs", {thingVar : thing});
+    res.render("love", {thingVar : thing});
 });
 
+app.get("/posts", function(req, res){
+    var posts = [
+        {title: "How to find a job without experience?", author: "Zaid"},
+        {title: "How to master cv writing?", author: "Sumaiya"},
+        {title: "How to ace an interview?", author: "Saleh"}
+    ]
+    res.render("posts", {posts: posts});
+});
 
 
 
