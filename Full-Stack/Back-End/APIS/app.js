9a40bc5a97727e604580b1b2c30ee999c69ab40a
Hussein <tejanahmedhu@gmail.com>
2020-02-09 23:42:47 +0000

    created API requests + using ES6 template + request & promise

--- Full-Stack/Back-End/APIS/app.js
+++ Full-Stack/Back-End/APIS/app.js
@@ -1,7 +1,13 @@
-const request = require('request');
-request('https://jsonplaceholder.typicode.com/users/1', (error, res, body)=>{
-    if(!error && res.statusCode == 200){
+// const request = require('request');
+
+const request_promise = require('request-promise');
+
+
+request_promise('https://jsonplaceholder.typicode.com/users/1')
+    .then((body)=>{
         const parsedData = JSON.parse(body);
         console.log(`${parsedData.name}  lives in  ${parsedData.address.city} on ${parsedData.address.street} in suite ${parsedData.address.suite} at ${parsedData.address.zipcode}`);
-    }
-});
+    })
+    .catch((err) => {
+        console.log('Error!', err);
+    })
