65b19feca422b6ad2ddf08ab7c2d3911ed024c74
Hussein <tejanahmedhu@gmail.com>
2020-02-24 15:25:30 +0000

    created dope EDIT & UPDATE routes

--- Full-Stack/Back-End/RestfulRouting/RESTfulBlogApp/app.js
+++ Full-Stack/Back-End/RestfulRouting/RESTfulBlogApp/app.js
@@ -1,13 +1,15 @@
-var express     = require("express"),
-    mongoose    = require("mongoose"),
-    bodyParser  = require("body-parser"),
-    app         = express();
+var express         = require("express"),
+    methodOverride  = require("method-override"),
+    mongoose        = require("mongoose"),
+    bodyParser      = require("body-parser"),
+    app             = express();
 
     //APP CONFIG
     mongoose.connect("mongodb://localhost/restful_blog_app");
     app.set("view engine", "ejs");
     app.use(express.static("public"));
     app.use(bodyParser.urlencoded({extended: true}));
+    app.use(methodOverride("_method"));
 
     //MONGOOSE/MODEL CONFIG
     var blogSchema = new mongoose.Schema({
@@ -69,8 +71,27 @@ var express     = require("express"),
         })
     });
 
-    //UPDATE ROUTE
+    //EDIT ROUTE
+    app.get("/blogs/:id/edit", function(req, res){
+        Blog.findById(req.params.id, function(error, foundBlog){
+            if(error){
+                res.redirect("/blogs");
+            } else{
+                res.render("edit", {blog: foundBlog});
+            }
+        });
+    });
 
+    //UPDATE ROUTE
+    app.put("/blogs/:id", function(req, res){
+        Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(error, updatedBlog){
+            if(error){
+                res.redirect("/blogs");
+            } else{
+                res.redirect("/blogs/" +req.params.id);
+            }
+        });
+    });
 
     //DELETE/DESTROY ROUTE
 
