3d497d9c027ecad098f2ac84d678bed661dd1d36
Hussein <tejanahmedhu@gmail.com>
2020-03-25 15:19:28 +0000

    modified comments section II

--- Full-Stack/Back-End/YelpCamp/v4/app.js
+++ Full-Stack/Back-End/YelpCamp/v4/app.js
@@ -2,7 +2,8 @@ var express     = require('express'),
     app         = express(), 
     bodyParser  = require("body-parser"), 
     mongoose    = require("mongoose"),
-    Campground  = require("./models/campground")
+    Campground  = require("./models/campground"),
+    Comment     = require("./models/comment");
     seedDB      = require("./seeds");
 
 
@@ -116,6 +117,33 @@ app.get("/campgrounds/:id/comments/new", function(req, res){
     });
 
 });
+
+app.post("/campgrounds/:id/comments", function(req, res){
+    //lookup campground via id
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else{
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                } else{
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            })
+        }
+    })
+    //create new comment
+
+    //connect new comment to campground
+
+    //redirect to show page
+
+    res.redirect
+})
 
 
 
